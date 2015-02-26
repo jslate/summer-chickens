0e16cf949454fdce339e4a47ca717ed07902a822
Jonathan Slate <jslate@patientslikeme.com>
2015-02-25 22:17:34 -0500

    allow special instructions in PDFs, keep chords from bunching up

--- app/pdfs/song_pdf.rb
+++ app/pdfs/song_pdf.rb
@@ -1,6 +1,7 @@
 class SongPdf < Prawn::Document
 
   CHORD_REGEX = /([^\[]*)\[([^\[]*)\]([^\[]*)/
+  INSTRUCTION_REGEX = /\A_(.*)_\s*\Z/
 
   def initialize(songs)
     super(top_margin: 40)
@@ -24,20 +25,22 @@ class SongPdf < Prawn::Document
   def lead_sheet(song)
     lines = song.lead_sheet.split(/\n/)
     lines.each_with_index do |line, index|
-      start_new_page if index > 0 && index % 20 == 0
-      move_down 40
+      start_new_page if index > 0 && index % 24 == 0
+      move_down(line.blank? ? 10 : 25)
       x = 0
       y_pos = y
-      if line.match CHORD_REGEX
+      if line.match INSTRUCTION_REGEX
+        text_box $1, at: [0, y], size: 10, style: :italic
+      elsif line.match CHORD_REGEX
         line.scan(CHORD_REGEX) do |start, chord, finish|
-          text_box start, at: [0, y]
-          x += width_of(start)
-          text_box chord, at: [x, y_pos + 15]
-          finish_box = text_box finish, at: [x, y_pos]
-          x += width_of(finish)
+          text_box start, at: [0, y], size: 10
+          x += width_of(start, size: 10)
+          text_box chord, at: [x, y_pos + 10], size: 10, style: :bold
+          finish_box = text_box finish, at: [x, y_pos], size: 10
+          x += [width_of(finish, size: 10), width_of(chord, size: 10) + 10].max
         end
       else
-        text_box line, at: [0, y]
+        text_box line, at: [0, y], size: 10
       end
     end
   end
